4b9d29d39584fda01467ded4969986f4b0568de7
Eleanor McHugh <eleanor@games-with-brains.com>
2016-10-22 08:53:08 +0100

    simple functions and object methods

---
+++ 04 functional interlude/04a.cr
@@ -0,0 +1,7 @@
+def print(s)
+	s.each_with_index do |v, i|
+		puts "#{i}: #{v}"
+	end
+end
+
+print [0, 2, 4, 6, 8]
---
+++ 04 functional interlude/04d.cr
@@ -0,0 +1,9 @@
+class Array
+	def print
+		self.each_with_index do |v, i|
+			puts "#{i}: #{v}"
+		end
+	end
+end
+
+[0, 2, 4, 6, 8].print
